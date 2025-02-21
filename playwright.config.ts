2242393608a9d75d4f7af7ebfb59cb46ce545fea
Carlos Ng <carlosng07@gmail.com>
2025-02-21 17:11:12 +0800

    config.ts - headless: true

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: [ ['html', { open: 'never' }], ['junit', { outputFile: 'test-results/junit-results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
